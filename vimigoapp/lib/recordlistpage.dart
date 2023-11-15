af47492c3e490da2451a9e52a1e700cc3095eb91
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2023-11-15 15:33:57 +0800

    Added in ListView for Firebase Added in Notification to user when scrolling to bottom and top of the listview Made Listview display according to timestamp in descending order ListView Display added in accordance to search bar so you can search the list view for items.

--- vimigoapp/lib/recordlistpage.dart
+++ vimigoapp/lib/recordlistpage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
@@ -13,6 +15,40 @@ class RecordList extends StatefulWidget {
 class _RecordListState extends State<RecordList> {
   String SearchVal = "";
   bool istime = true;
+  final stuff =  FirebaseFirestore.instance.collection('Users').orderBy('check-in',descending:true).snapshots();
+  late ScrollController _controller ;
+
+  _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      Fluttertoast.showToast(
+                  msg: 'Reached the bottom!',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.grey,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      Fluttertoast.showToast(
+                  msg: 'Reached the top!',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.grey,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+    }
+ }
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +85,113 @@ class _RecordListState extends State<RecordList> {
         child: Icon(Icons.hourglass_empty),
       ),
 
+      body: StreamBuilder(
+        stream: stuff , 
+        builder: (context, snapshot){
+          if (snapshot.hasError){
+            return const Text('connection error');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return const Text('loading');
+          }
+
+          var docs = snapshot.data!.docs;
+
+          String getdate(var docs,int index){
+            final DateTime currentTime = DateTime.now();
+            String displayString = "";
+                    
+                    
+            DateTime tempDate = new DateFormat('yyyy-MM-dd hh:mm:ss').parse(docs[index]['check-in']) ;
+                    
+            final timeDiff = currentTime.difference(tempDate);
+
+                    
+
+            final days = timeDiff.inDays; // find the num of days
+            final hours = timeDiff.inHours % 24; // find the num of hours ago
+            final minutes = timeDiff.inMinutes % 60; // find the num of mins ago
+            final seconds = timeDiff.inSeconds % 60; // find the num of seconds
+            displayString = (days > 0 ? days.toString() + "d " : "") +
+                            (hours > 0 ? hours.toString() + "hr " : "") +
+                            (minutes > 0 ? minutes.toString() + "min " : "") +
+                            (seconds > 0 ? seconds.toString() + "s " : "") +
+                            "ago";
+            return displayString;
+          }
+                    
+                    
+                    
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.separated(
+              shrinkWrap: true,
+              controller: _controller,
+              itemCount: docs.length, 
+              itemBuilder: (context,index){
+                if (SearchVal.isEmpty){
+                  return ListTile(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                      bottomRight: Radius.circular(25),
+                      bottomLeft: Radius.circular(25))
+                      ),
+                    tileColor: Colors.blue[300],
+                    title: Text(docs[index]['user']),
+                    subtitle: Text(docs[index]['phone']),
+                    trailing: istime ? ( Text( getdate(docs,index)) ) : Text(docs[index]['check-in']),
+                
+                  );
+                }
+                if (docs[index]['user'].toLowerCase().startsWith(SearchVal.toLowerCase())){
+                    return ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25))
+                        ),
+                      tileColor: Colors.blue[300],
+                      title: Text(docs[index]['user']),
+                      subtitle: Text(docs[index]['phone']),
+                      trailing: istime ? ( Text( getdate(docs,index)) ) : Text(docs[index]['check-in']),
+                  );
+                
+                }
+                else if (docs[index]['phone'].toLowerCase().startsWith(SearchVal.toLowerCase())){
+                    return ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25))
+                        ),
+                      tileColor: Colors.blue[300],
+                      title: Text(docs[index]['user']),
+                      subtitle: Text(docs[index]['phone']),
+                      trailing: istime ? ( Text( getdate(docs,index)) ) : Text(docs[index]['check-in']),
+                      
+                  );
+                }
+                else{
+                    return Container();
+                }
+                          
+              },
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+                      
+                        
+            ),
+          );
+        },
+                  
+      )
     );
   }
 }
